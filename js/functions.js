ebd02d0d3fe50fb7903b64178c6d12f688c768cb
DenisMarcov <95167521+DenisMarcov@users.noreply.github.com>
2025-12-14 22:31:52 +0400

    module5-task1

--- js/functions.js
+++ js/functions.js
@@ -28,3 +28,29 @@ function extractNumbers (str) {
 }
 
 extractNumbers(stri3);
+
+// module5-task2
+function checkTimeMeeting (startWorkDay, endWorkDay, timeMeeting, durationMeeting) {
+  const [startHours, startMinutes] = startWorkDay.split(':').map(Number);
+  const [endHours, endMinutes] = endWorkDay.split(':').map(Number);
+  const [meetingHours, meetingMinutes] = timeMeeting.split(':').map(Number);
+
+  let flag = true;
+
+  if (startHours <= meetingHours && startMinutes <= meetingMinutes && endHours >= meetingHours && endMinutes >= meetingMinutes){
+    const durationMeetingHours = Math.floor(durationMeeting / 60);
+    const durationMeetingMinutes = durationMeeting % 60;
+    if (meetingHours + durationMeetingHours > endHours){
+      flag = false;
+      return flag;
+    } else if (meetingHours + durationMeetingHours === endHours && meetingMinutes + durationMeetingMinutes > endMinutes){
+      flag = false;
+      return flag;
+    }
+  } else {
+    flag = false;
+    return flag;
+  }
+  return flag;
+}
+console.log(checkTimeMeeting('8:00', '17:30', '08:00', 900));
